073402f8e242e1c9a01a9b465e3bcbf19c1ac876
Garrett Gregor <garrett.gregor@gmail.com>
2024-09-25 11:20:05 -0600

    update per rubocop styling

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,20 +1,26 @@
 class Cipher
-  UPPER_CHARS = ("A".."Z").to_a
-  LOWER_CHARS = ("a".."z").to_a
+  UPPER_CHARS = ('A'..'Z').to_a
+  LOWER_CHARS = ('a'..'z').to_a
 
   def caesar_cipher(string, shift_input)
     string.chars.map do |char|
       if UPPER_CHARS.include?(char)
         start_index = UPPER_CHARS.index(char)
         end_index = start_index + shift_input
-        end_index > UPPER_CHARS.length ?
-          end_index = end_index - UPPER_CHARS.length : end_index
+        if end_index > UPPER_CHARS.length
+          end_index -= UPPER_CHARS.length
+        else
+          end_index
+        end
         UPPER_CHARS[end_index]
       elsif LOWER_CHARS.include?(char)
         start_index = LOWER_CHARS.index(char)
         end_index = start_index + shift_input
-        end_index > LOWER_CHARS.length ?
-          end_index = end_index - LOWER_CHARS.length : end_index
+        if end_index > LOWER_CHARS.length
+          end_index -= LOWER_CHARS.length
+        else
+          end_index
+        end
         LOWER_CHARS[end_index]
       else
         char
--- spec/cipher_spec.rb
+++ spec/cipher_spec.rb
@@ -1,11 +1,11 @@
 require './lib/cipher'
 
 RSpec.describe Cipher do
-  let!(:cipher) {Cipher.new}
+  let!(:cipher) { Cipher.new }
 
-  describe "instance methods" do
-    it "encrypts a string using the caesar cipher method and an input" do
-      expect(cipher.caesar_cipher("What a string!", 5)).to eq("Bmfy f xywnsl!")
+  describe 'instance methods' do
+    it 'encrypts a string using the caesar cipher method and an input' do
+      expect(cipher.caesar_cipher('What a string!', 5)).to eq('Bmfy f xywnsl!')
     end
   end
 end
